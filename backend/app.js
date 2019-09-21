d6821f80c86fa2df98d99defe4043bbe74f8ea1a
Ahsa <ahsa@ahsas-mbp.attlocal.net>
2019-09-20 17:21:26 -0700

    adding get request functionality

--- backend/app.js
+++ backend/app.js
@@ -38,20 +38,19 @@ app.post("/api/posts", (req, res, next) => {
 });
 
 app.get('/api/posts', (req, res, next) => {
-  const posts = [
-    { id: 'asdfa123411',
-    title: 'First server-side post',
-    content: 'This is coming from the server'
-    },
-    { id: 'asdfa123f',
-    title: 'Second server-side post',
-    content: 'This is coming from the server!'
-    },
-  ];
-  res.status(200).json({
-    message: 'Posts fetched succesfully!',
-    posts: posts
-  });
+  Post.query(
+    'SELECT * FROM posts',
+    (err,results) => {
+      if (err) {
+        console.log(err.stack)
+      } else {
+        res.status(200).json({
+          message: "Posts fetched successfully!",
+          posts: results.rows
+        })
+      }
+    }
+  )
 });
 
 module.exports = app;
