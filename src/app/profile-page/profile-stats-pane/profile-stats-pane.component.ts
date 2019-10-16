a7760697abde4e33f51b56c5761b7089288130ae
Oisin Coveney <oisincoveney@gmail.com>
2019-10-16 16:57:01 -0700

    T3-3 Added spacing and some logic to stats pane

--- src/app/profile-page/profile-stats-pane/profile-stats-pane.component.ts
+++ src/app/profile-page/profile-stats-pane/profile-stats-pane.component.ts
@@ -1,15 +1,56 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-profile-stats-pane',
-  templateUrl: './profile-stats-pane.component.html',
-  styleUrls: ['./profile-stats-pane.component.css']
+    selector: 'app-profile-stats-pane',
+    templateUrl: './profile-stats-pane.component.html',
+    styleUrls: ['./profile-stats-pane.component.scss']
 })
 export class ProfileStatsPaneComponent implements OnInit {
 
-  constructor() { }
+    // private stats: {}[];
+    // noinspection JSMismatchedCollectionQueryUpdate
+    private fakeData: {}[] = [
+        {
+            title: 'goals',
+            value: 10
+        },
+        {
+            title: 'saves',
+            value: 420
+        },
+        {
+            title: 'Favorite Candy',
+            value: 'Deez'
+        },
+        {
+            title: 'goals',
+            value: 10
+        },
+        {
+            title: 'saves',
+            value: 420
+        },
+        {
+            title: 'Favorite Candy',
+            value: 'Deez'
+        },
+        {
+            title: 'goals',
+            value: 10
+        },
+        {
+            title: 'saves',
+            value: 420
+        },
+        {
+            title: 'Favorite Candy',
+            value: 'Deez'
+        }
+    ];
 
-  ngOnInit() {
-  }
+    constructor() {
+    }
 
+    ngOnInit() {
+    }
 }
