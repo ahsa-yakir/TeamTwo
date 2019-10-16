c9e5dad26579bab136ad810f6ef96c86eca6abbd
Oisin Coveney <oisincoveney@gmail.com>
2019-10-16 10:00:34 -0700

    T3-3 added material cards and sections for profile page

---
+++ src/app/profile-page/profile-stats-pane/profile-stats-pane.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-stats-pane',
+  templateUrl: './profile-stats-pane.component.html',
+  styleUrls: ['./profile-stats-pane.component.css']
+})
+export class ProfileStatsPaneComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
