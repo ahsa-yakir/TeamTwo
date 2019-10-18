7e3bd0e5b558992139fe3d4d240d1fdea738b5f7
Aaron Francis <aaronfrancis1414@gmail.com>
2019-10-17 18:34:16 -0700

    redirect on login/signup. added spinner for loading

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {AuthData} from './auth-data';
 import {Subject} from 'rxjs';
+import {Router} from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -31,7 +32,7 @@ export class AuthService {
         });
   }
 
-  login(email:string, password: string){
+  login(email: string, password: string){
       const authData: AuthData = {email, password};
       this.http.post<{token: string}>("http://localhost:3000/api/user/login", authData)
           .subscribe(response => {
@@ -40,14 +41,16 @@ export class AuthService {
               if (token) {
                   this.isAuthenticated = true;
                   this.authStatusListener.next(true);
+                  this.router.navigate(['/']);
               }
           });
   }
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private router: Router) {}
 
   logout() {
         this.token = null;
         this.isAuthenticated = false;
         this.authStatusListener.next(false);
+        this.router.navigate(['/']);
   }
 }
