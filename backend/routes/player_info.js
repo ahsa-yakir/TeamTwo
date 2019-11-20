72323adde75e5faf84eefc5b23c4a6237fcd6253
ahsa-yakir1 <bryanna.valdivia@sjsu.edu>
2019-11-20 10:04:04 -0800

    message

--- backend/routes/player_info.js
+++ backend/routes/player_info.js
@@ -1,25 +1,37 @@
 const express = require('express');
 const models = require('../models');
 const router = express.Router();
-const Player_info = models.player_info;
+const multer = require('multer');
+const getFields = multer();
+const checkAuth = require('../middleware/check-auth');
 
+const Player_info = models.player_info;
 
 router.get('', (req, res, next) => {
     Player_info.findAll({
-        attributes: ['user_id', 'name', 'last_login', 'birthdate', 'high_school', 'profile_picture',
-    'city', 'state', 'country'],
+        attributes: [
+            'user_id',
+            'name',
+            'last_login',
+            'birthdate',
+            'high_school',
+            'profile_picture',
+            'city',
+            'state',
+            'country',
+        ],
     })
-         .catch(err => {
-             res.status(500).json({
+        .catch(err => {
+            res.status(500).json({
                 error: err,
-             });
-         })
-         .then(result => {
-             res.status(201).json({
+            });
+        })
+        .then(result => {
+            res.status(201).json({
                 message: 'Player_info fetched successfully',
                 player_info: result,
-             });
-         });
+            });
+        });
 });
 
 router.get('/:user_id', (req, res, next) => {
@@ -45,8 +57,86 @@ router.get('/:user_id', (req, res, next) => {
                 state: results[0].dataValues.state,
                 country: results[0].dataValues.country,
             });
-        });  
+        });
 });
 
+router.post(
+    '/:add',
+    getFields.any(),
+    checkAuth,
+    ///:add added for testing purposes
+    (req, res, next) => {
+        Player_info.create({
+            user_id: req.userData.user_id,
+            name: req.body.name,
+            last_login: req.body.last_login,
+            birthdate: req.body.birthdate,
+            high_school: req.body.high_school,
+            profile_picture: req.body.profile_picture,
+            city: req.body.city,
+            state: req.body.state,
+            country: req.body.country,
+        })
+            .catch(err => {
+                res.status(500).json({
+                    error: err,
+                });
+            })
+            .then(results => {
+                res.status(201).json({
+                    message: 'Player Info Added!',
+                    player_info: {
+                        user_id: results.dataValues.user_id,
+                        name: results.dataValues.name,
+                        last_login: results.dataValues.last_login,
+                        birthdate: results.dataValues.birthdate,
+                        high_school: results.dataValues.high_school,
+                        profile_picture: results.dataValues.profile_picture,
+                        city: results.dataValues.city,
+                        state: results.dataValues.state,
+                        country: results.dataValues.country,
+                    },
+                });
+            });
+    }
+);
+
+//update
+router.put('/:user_id', getFields.any(), checkAuth, (req, res, next) => {
+    Player_info.update(
+        {
+            user_id: req.userData.user_id,
+            name: req.body.name,
+            last_login: req.body.last_login,
+            birthdate: req.body.birthdate,
+            high_school: req.body.high_school,
+            profile_picture: req.body.profile_picture,
+            city: req.body.city,
+            state: req.body.state,
+            country: req.body.country,
+        },
+        {
+            where: {
+                user_id: req.userData.user_id,
+            },
+        }
+    )
+        .catch(err => {
+            res.status(500).json({
+                error: err,
+            });
+        })
+        .then(result => {
+            if (result[0] > 0) {
+                res.status(200).json({
+                    message: 'Player Info updated successfully',
+                });
+            } else {
+                res.status(401).json({
+                    message: 'Not Authorized!',
+                });
+            }
+        });
+});
 
 module.exports = router;
--- backend/routes/stats.js
+++ backend/routes/stats.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const models = require('../models');
 const router = express.Router();
+const multer = require('multer');
+const getFields = multer();
+const checkAuth = require('../middleware/check-auth');
+
 const Stat = models.stats;
 
 router.get('/:user_id', (req, res, next) => {
@@ -28,17 +32,82 @@ router.get('', (req, res, next) => {
     Stat.findAll({
         attributes: ['goals', 'assists', 'games_played', 'user_id'],
     })
-         .catch(err => {
-             res.status(500).json({
-                 error: err,
-             });
+        .catch(err => {
+            res.status(500).json({
+                error: err,
+            });
         })
-         .then(result => {
-             res.status(201).json({
+        .then(result => {
+            res.status(201).json({
                 message: 'Stats fetched successfully',
                 stats: result,
-             });
-         });
+            });
+        });
+});
+
+router.post(
+    '/:add',
+    getFields.any(),
+    checkAuth,
+    ///:add added for testing
+    (req, res, next) => {
+        Stats.create({
+            goals: req.body.goals,
+            assists: req.body.assists,
+            games_played: req.body.games_played,
+            user_id: req.userData.user_id,
+        })
+            .catch(err => {
+                res.status(500).json({
+                    error: err,
+                });
+            })
+            .then(results => {
+                res.status(201).json({
+                    message: 'Stats Added!',
+                    stats: {
+                        goals: results.dataValues.goals,
+                        assists: results.dataValues.assists,
+                        games_played: results.dataValues.games_played,
+                        user_id: results.dataValues.user_id,
+                    },
+                });
+            });
+    }
+);
+
+//update
+router.put('/:user_id', getFields.any(), checkAuth, (req, res, next) => {
+    Stats.update(
+        {
+            goals: req.body.goals,
+            assists: req.body.assists,
+            games_played: req.body.games_played,
+            user_id: req.userData.user_id,
+        },
+        {
+            where: {
+                user_id: req.userData.user_id,
+                //or user_id: req.userData.userId ?
+            },
+        }
+    )
+        .catch(err => {
+            res.status(500).json({
+                error: err,
+            });
+        })
+        .then(result => {
+            if (result[0] > 0) {
+                res.status(200).json({
+                    message: 'Stats updated successfully',
+                });
+            } else {
+                res.status(401).json({
+                    message: 'Not Authorized!',
+                });
+            }
+        });
 });
 
 module.exports = router;
