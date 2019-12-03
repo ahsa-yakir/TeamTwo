01708e865e36ff5874fd531c7e88e53afbc63fe5
Ahsa Yakir <ahsayakir.yakir@sjsu.edu>
2019-12-03 13:13:38 -0800

    create profile initial push

---
+++ src/app/create-profile/create-profile.model.ts
@@ -0,0 +1,10 @@
+export interface createProfile {
+    firstName: string;
+    lastName: string;
+    birthdate: Date;
+    highSchool: string;
+    city: string;
+    state: string;
+    country: string;
+    user_id: string;
+}
